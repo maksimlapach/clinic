ab3a821daed9788db38ae697b660af37d55dd2e9
Maksym <maksimlapach@gmail.com>
2023-05-30 13:00:16 +0300

    Finish helper section

--- dist/js/script.js
+++ dist/js/script.js
@@ -35,6 +35,15 @@ const sliders = () => {
     'navContainer': ".promotions .slider__nav",
     "navAsThumbnails": true
   });
+  (0,tiny_slider_src_tiny_slider__WEBPACK_IMPORTED_MODULE_0__.tns)({
+    "container": '.doctors .slider__wrapper',
+    "items": 1,
+    "swipeAngle": false,
+    "speed": 400,
+    "controlsContainer": ".doctors .slider__controls",
+    'navContainer': ".doctors .slider__nav",
+    "navAsThumbnails": true
+  });
 };
 /* harmony default export */ __webpack_exports__["default"] = (sliders);
 
--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -23,6 +23,15 @@ const sliders = () => {
         'navContainer': ".promotions .slider__nav",
         "navAsThumbnails": true
     });
+    tns({
+        "container": '.doctors .slider__wrapper',
+        "items": 1,
+        "swipeAngle": false,
+        "speed": 400,
+        "controlsContainer": ".doctors .slider__controls",
+        'navContainer': ".doctors .slider__nav",
+        "navAsThumbnails": true
+    });
 };
 
 export default sliders;
